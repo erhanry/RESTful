8ec22475adc20f2a4a4c9b498b1772a25b087b05
Erhan Ysuf <erhan.ysuf@gmail.com>
2024-07-24 04:38:46 +0300

    Correction to Access-Control-Allow-Origin

--- src/middlewares/corsMiddleware.js
+++ src/middlewares/corsMiddleware.js
@@ -1,6 +1,6 @@
 export const corsMiddleware = () => (req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+    res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
+    res.setHeader("Access-Control-Allow-Methods", "GET, HEAD, POST, PUT, DELETE, CONNECT, OPTIONS, PATCH");
     res.setHeader("Access-Control-Allow-Credentials", true);
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, X-Authorization");
 
